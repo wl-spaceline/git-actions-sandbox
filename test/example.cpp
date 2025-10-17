1b9beeb2c519232d66c40ddd1b52a9fe321e6fae
William Ledda <wl@spacelinelabs.com>
2025-10-17 13:04:32 +0200

    Experiment test pipeline - A test will fail

---
+++ test/example.cpp
@@ -0,0 +1,14 @@
+//
+// Created by wlspace on 10/17/25.
+//
+#include <gtest/gtest.h>
+
+TEST(Example, DummySuccess)
+{
+    ASSERT_TRUE(true);
+}
+
+TEST(Example, DummyFailure)
+{
+    ASSERT_FALSE(false);
+}
